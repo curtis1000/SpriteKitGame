fb366f0d1b61bb9e788edc6d9267605e1f40c1d9
cbranum <tr1uv13q>
2014-02-06 18:06:30 -0600

    initial commit

---
+++ SpriteKitGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitGame
+//
+//  Created by Curtis Branum on 2/6/14.
+//  Copyright (c) 2014 Morsekode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitGame/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SpriteKitGame
+//
+
+//  Copyright (c) 2014 Morsekode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
